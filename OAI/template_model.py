c8fcac328d2ce4aa4536680334c5eb96d8c050c5
jingkaifu <jf3516@columbia.edu>
2024-05-10 18:14:11 -0400

    debugged try catch

--- OAI/template_model.py
+++ OAI/template_model.py
@@ -63,17 +63,21 @@ class DeepLearningModel(nn.Module):
         time_backward_prop = 0
         time_update_step = 0
 
-        skipped_iterations = 0
+        samples_consumed = 0
         # Iterate over data.
         # keep track of all labels + outputs to compute the final metrics.
+        
         concatenated_labels = {}
         concatenated_outputs = {}
         loss_details = []
         for data in dataloaders[phase]:
             # print("We reached the beginning of the loop with %i images" % n_batches_loaded)
-            if any(sample is None for sample in data):
-                skipped_iterations += 1
+            collated_batch, num_valid_samples = data
+            if num_valid_samples == 0:
+                # Skip the batch if all samples are None
                 continue
+            samples_consumed += num_valid_samples
+            data = collated_batch
             t = time.time()
             n_batches_loaded += 1
             if n_batches_loaded % 100 == 0:
@@ -151,7 +155,7 @@ class DeepLearningModel(nn.Module):
             'epoch_loss': epoch_loss,
             'loss_details': loss_details,
         }
-        print(f"skipped iterations in {phase}: {skipped_iterations}")
+        print(f"skipped iterations in {phase}: {len(dataloaders[phase]) - samples_consumed}")
         metrics_for_epoch = self.analyse_predictions(concatenated_labels, concatenated_outputs, info)
         return metrics_for_epoch
 
